5ee707823b4d5ea50e1813d6e8949ff532826983
ANN LIN <smilemarg@gmail.com>
2019-10-06 21:43:57 -0400

    navbar mock up

--- src/app.js
+++ src/app.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import {Sidebar, Main} from './components'
+import {Sidebar, Main, Navbar} from './components'
 
 const App = () => {
   return (
     <div id='app-container'>
       <Sidebar />
+      <Navbar />
       <Main />
     </div>
   )
--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,6 @@ import {Navbar} from '../components'
 const Main = () => {
   return(
     <div id='main'>
-      <Navbar />
       THIS IS MAIN CONTENT!
     </div>
   )
