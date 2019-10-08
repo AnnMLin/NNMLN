5b0c11c8984beced691a912b92413fb8542a9a24
ANN LIN <smilemarg@gmail.com>
2019-10-08 00:39:27 -0400

    homepage styled

--- src/app.js
+++ src/app.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import {Sidebar, Main, Navbar} from './components'
+import {Sidebar, Home} from './components'
 
 const App = () => {
   return (
     <div id='app-container'>
       <Sidebar />
-      <Navbar />
-      <Main />
+      <Home />
     </div>
   )
 }
--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
 export {default as Sidebar} from './Sidebar.js'
 export {default as Navbar} from './Navbar.js'
-export {default as Main} from './Main.js'
+export {default as Home} from './Home.js'
 
