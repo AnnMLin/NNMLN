4d1f9c8d6e52ed31c966e8013f9b6e945aeb00cb
ANN LIN <smilemarg@gmail.com>
2020-04-12 20:34:03 -0400

    icon test

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -8,7 +8,7 @@ const Sidebar = () => {
           <img className='quick-link-icon' src={process.env.PUBLIC_URL + "/icons/github-5.png"} alt='link to Github'/>
         </a>
         <a className='quick-link' href='https://www.linkedin.com/in/ann-m-lin' target='_blank'>
-          <img className='quick-link-icon' src={process.env.PUBLIC_URL + "/icons/github-5.png"} alt='link to Linkedin'/>
+          <img className='quick-link-icon' src={process.env.PUBLIC_URL + "/icons/linkedin-4.png"} alt='link to Linkedin'/>
         </a>
         <a className='quick-link' href='' target='_blank'>
           <img className='quick-link-icon' src={process.env.PUBLIC_URL + "/icons/download-2.png"} alt='link to Resume'/>
