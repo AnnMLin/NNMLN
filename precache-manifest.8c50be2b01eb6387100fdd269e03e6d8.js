c2043c32b21d5f8a5a5ac34f028a71c8b85395dd
ANN LIN <smilemarg@gmail.com>
2020-05-02 23:18:51 -0400

    Updates

---
+++ precache-manifest.8c50be2b01eb6387100fdd269e03e6d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "df925de5f159fac6e482d5774300492f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1d4b2fa8440ed6e934fb",
+    "url": "/static/js/2.b6e38b29.chunk.js"
+  },
+  {
+    "revision": "f8cc39663d7ac784d34bf6adee7d4fb1",
+    "url": "/static/js/2.b6e38b29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e604cf172f05fd7c5830",
+    "url": "/static/js/main.47d50ea6.chunk.js"
+  },
+  {
+    "revision": "d184dc042891a295d26e",
+    "url": "/static/js/runtime-main.1823a481.js"
+  }
+]);
